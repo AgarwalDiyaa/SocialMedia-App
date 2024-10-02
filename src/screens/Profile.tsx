e0a072232ec0b888b85277a16b1e1e59b465cc30
Abdul Khader Wali <abdulkhader@gigandtake.com>
2024-10-02 21:20:21 +0530

    feat : login page and logout

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,14 +1,28 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+import auth from '@react-native-firebase/auth';
+import { styles } from '../styles';
 import React from 'react'
 
-const Profile = () => {
+const Profile = ({setUser}: any) => {
   return (
     <View>
       <Text>Profile</Text>
+      {/* Logout button */}
+      <TouchableOpacity
+        onPress={() => {
+          // Logout
+          auth()
+            .signOut()
+            .then(() => {
+              setUser(null);
+              console.log('User signed out!')});
+        }}
+        style={styles.loginButton}
+      >
+        <Text style={styles.loginText}>Logout</Text>
+      </TouchableOpacity>
     </View>
   )
 }
 
 export default Profile
-
-const styles = StyleSheet.create({})
